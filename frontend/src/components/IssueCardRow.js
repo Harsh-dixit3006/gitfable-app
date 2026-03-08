9beffcdc88b86b01b7c081e0224624edd3b3771d
Nishant Gajjar <nishantkgajjar@gmail.com>
2026-03-08 02:29:27 +0100

    Updated InfoSidebar.js

--- frontend/src/components/IssueCardRow.js
+++ frontend/src/components/IssueCardRow.js
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { ExternalLink, Star, Crosshair, Code2 } from 'lucide-react';
+import { ExternalLink, Star, Crosshair, Code as Code2 } from 'lucide-react';
 import { Badge } from '@/components/ui/badge';
 import { RARITY, DIFF_COLORS, RarityBadge } from '@/components/DrawAnimation';
 
